0a2bd9b4714d8ddc39d25cb8812c8337096f0833
ThieresCA <89056548+ThieresCA@users.noreply.github.com>
2023-11-09 19:48:19 -0300

    criando o projeto e adicionando dependencias

---
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -0,0 +1,15 @@
+package com.example.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+@SpringBootApplication
+public class CardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class, args);
+	}
+
+}
