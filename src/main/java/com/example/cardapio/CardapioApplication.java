984bfb5cb5fb74a3b1ff83d1a3055284cdfd1175
Author Name <thieresca@gmail.com>
2023-11-16 11:42:32 -0300

    adicionando o swagger

--- src/main/java/com/example/cardapio/CardapioApplication.java
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -1,10 +1,11 @@
 package com.example.cardapio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "API desenvolvida para testes do OpenApi"))
 public class CardapioApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -3,17 +3,25 @@ package com.example.cardapio.controller;
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
 import com.example.cardapio.food.FoodResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 //dizendo que essa é uma classe de controller
 @RestController
+@Slf4j
 //mapeando qual o endpoint que vai chamar esse controller
-@RequestMapping("/food")
+@RequestMapping(value = "/food", produces = {"application/json"})
+@Tag(name = "Food-Controller")
 public class FoodController {
 
     //injetando dependência
@@ -22,18 +30,37 @@ public class FoodController {
 
     //indicando que quando vier o endpoint "food" com o método GET esse será o método acionado
     @GetMapping()
+    @Operation(summary = "Realiza a consulta de todas as comidas", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A inserção foi realizada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro ao procurar pelos dados"),
+    })
     public List<FoodResponseDto> getAll() {
         List<FoodResponseDto> foodList = repository.findAll().stream().map(FoodResponseDto::new).toList();
         return foodList;
     }
 
     @GetMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A inserção foi realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao procurar pelos dados"),
+    })
+    @Operation(summary = "Realiza a consulta especifica de uma comida com o determina ID", method = "GET")
     public List<FoodResponseDto> getById(@PathVariable Long id) {
         List<FoodResponseDto> food = repository.findById(id).stream().map(FoodResponseDto::new).toList();
         return food;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    //@Operation(summary = "Realiza as inserções das comidas", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A inserção foi realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar a inserção dos dados"),
+    })
     public HttpStatusCode insertFood(@RequestBody Food food) {
         try {
             repository.save(food);
@@ -44,8 +71,15 @@ public class FoodController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Realiza a atualização das comidas", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A atualização foi realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar a atualização dos dados"),
+    })
     public HttpStatusCode attFood(@RequestBody Food food, @PathVariable Long id) {
-        var foodList = repository.findById(id);
+        Optional foodList = repository.findById(id);
         if (foodList.isEmpty()) {
             throw new Error("o item que está procurando não existe");
         } else {
@@ -56,6 +90,13 @@ public class FoodController {
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Realiza a remoção da comida especificada", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A remoção foi realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar a remoção dos dados"),
+    })
     public HttpStatusCode deleteFood(@PathVariable Long id) {
         List<Food> foodList = repository.findById(id).stream().toList();
         if (foodList.isEmpty()) {
